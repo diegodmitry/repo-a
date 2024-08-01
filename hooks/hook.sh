cb7047f86ede02cbc7f28057d52fed361f81752b
Diego Dmitry <diegodmitry@hotmail.com>
2024-08-01 11:32:37 +0100

    repository mirroring

---
+++ hooks/hook.sh
@@ -0,0 +1,8 @@
+# fetch from original repositroy
+# git fetch origin
+
+# This mirrors repo-a to repo-b after fetching updates.
+# git push --mirror mirror
+
+# Update from local repo to remote repo
+# git push
